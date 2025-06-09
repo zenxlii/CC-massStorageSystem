1b1a449c2da0f3a0935a1425b0e73d094896f7ab
zenxlii <chloej4b6@gmail.com>
2025-06-09 14:52:17 +0100

    Update config.lua
    Added the initial groundwork for the item condensing functionality that is slated to be added to the server program.

--- codebase/common/config.lua
+++ codebase/common/config.lua
@@ -78,6 +78,17 @@ if modemSide == "" then
 	error("Please attach a modem to this turtle.")
 end
 
+--Table of items that should be
+--auto-condensed by the server program.
+--The sub-table structure is as
+--follows:
+--1. Input encoded name.
+--2. Recipe type.
+--3. Output encoded name.
+--4. Maximum batch craft size.
+local condenseTable = {}
+table.insert(condenseTable, {"minecraft:redstone", "3x3", "minecraft:redstone_block", 64})
+
 --Return Block
 return{
 batchSize = batchSize,
@@ -91,5 +102,6 @@ self = self,
 clientExportBuffer = clientExportBuffer,
 requestsDir = requestsDir,
 modemSide = modemSide,
-recipesDir = recipesDir
+recipesDir = recipesDir,
+condenseTable = condenseTable
 }
