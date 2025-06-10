fbf18d9ddf740a7ec2945f9b475c00711b7cae65
zenxlii <chloej4b6@gmail.com>
2025-06-10 11:35:34 +0100

    Update mssServerConfig.lua
    Yet more condensing recipes got added.

--- codebase/server/mssServerConfig.lua
+++ codebase/server/mssServerConfig.lua
@@ -14,6 +14,9 @@
 local condenseTable = {}
 table.insert(condenseTable, {"minecraft:redstone", "3x3", "minecraft:redstone_block", 64})
 table.insert(condenseTable, {"minecraft:lapis_lazuli", "3x3", "minecraft:lapis_block", 64})
+table.insert(condenseTable, {"minecraft:emerald", "3x3", "minecraft:emerald_block", 64})
+table.insert(condenseTable, {"minecraft:diamond", "3x3", "minecraft:diamond_block", 64})
+table.insert(condenseTable, {"minecraft:coal", "3x3", "minecraft:coal_block", 64})
 table.insert(condenseTable, {"minecraft:iron_nugget", "3x3", "minecraft:iron_ingot", 64})
 table.insert(condenseTable, {"minecraft:iron_ingot", "3x3", "minecraft:iron_block", 64})
 table.insert(condenseTable, {"minecraft:gold_nugget", "3x3", "minecraft:gold_ingot", 64})
