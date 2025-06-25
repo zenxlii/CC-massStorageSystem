b544b0ac844666451371fc8e117b1797f9f0663c
zenxlii <chloej4b6@gmail.com>
2025-06-25 16:36:40 +0100

    Update recipeListAssembler.lua
    Forgot that `fileName` included ".lua" at the end.

--- codebase/server/recipeListAssembler.lua
+++ codebase/server/recipeListAssembler.lua
@@ -17,7 +17,7 @@ if recipeFiles ~= {} then
 	--and disconnect them from their
 	--source library.
 	for index, fileName in ipairs(recipeFiles) do
-		local l = require(dir.."."..fileName)
+		local l = require(dir.."."..string.sub(fileName,1,-5))
 		local recipeList = textutils.unserialise(textutils.serialise(l.recipeList))
 		--Tricky little memory saving?
 		l = nil
