2246ce7a785020200f7492d2e8c625001dcf885f
zenxlii <chloej4b6@gmail.com>
2025-08-23 15:29:11 +0100

    Renamed stuff

---
+++ codebase/modularServer/mssConfigLinker.lua
@@ -0,0 +1,5 @@
+--This file acts as a bridge between
+--the parts of MSS's modular server
+--parts and libraries held on external
+--disk drives.
+local config = require("REPLACE DURING INSTALLATION")
